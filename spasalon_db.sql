b608a0b1e2ff41b8bccea0be1b1c924f2166fdfa
ProbablyNotBestUsername <ubnubnhub@gmail.com>
2025-09-29 08:51:09 +0500

    update database dump

--- spasalon_db.sql
+++ spasalon_db.sql
@@ -1,19 +1,27 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.4.6, for Win64 (x86_64)
 --
--- Host: localhost    Database: spa_salon
+-- Host: mysql-whatever-livinitlarge-4d71.f.aivencloud.com    Database: spasalon
 -- ------------------------------------------------------
--- Server version	8.0.40
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '1543d9f2-985b-11f0-b1c0-ae2d1403823e:1-81';
 
 --
 -- Table structure for table `appointments`
@@ -24,18 +32,21 @@ DROP TABLE IF EXISTS `appointments`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `appointments` (
   `AppointmentId` int NOT NULL AUTO_INCREMENT,
-  `ClientId` int NOT NULL,
-  `ServiceId` int NOT NULL,
+  `clientId` int NOT NULL,
+  `staffId` int NOT NULL,
+  `serviceId` int NOT NULL,
   `AppointmentDate` datetime NOT NULL,
   `EndTime` datetime NOT NULL,
   `Status` varchar(20) DEFAULT 'Scheduled',
   `CreatedAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `UpdatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`AppointmentId`),
-  KEY `ClientId` (`ClientId`),
-  KEY `ServiceId` (`ServiceId`),
-  CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`ClientId`) REFERENCES `users` (`UserId`),
-  CONSTRAINT `appointments_ibfk_3` FOREIGN KEY (`ServiceId`) REFERENCES `services` (`ServiceId`),
+  KEY `ClientId` (`clientId`),
+  KEY `ServiceId` (`serviceId`),
+  KEY `appointments_staff_staffId_fk` (`staffId`),
+  CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `client` (`clientId`) ON UPDATE CASCADE,
+  CONSTRAINT `appointments_ibfk_3` FOREIGN KEY (`serviceId`) REFERENCES `services` (`ServiceId`),
+  CONSTRAINT `appointments_staff_staffId_fk` FOREIGN KEY (`staffId`) REFERENCES `staff` (`staffId`) ON UPDATE CASCADE,
   CONSTRAINT `appointments_chk_1` CHECK ((`Status` in (_utf8mb4'Scheduled',_utf8mb4'Completed',_utf8mb4'Cancelled',_utf8mb4'NoShow')))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -47,6 +58,33 @@ CREATE TABLE `appointments` (
 LOCK TABLES `appointments` WRITE;
 /*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
 /*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client` (
+  `clientId` int NOT NULL AUTO_INCREMENT,
+  `clientName` varchar(100) NOT NULL,
+  `clientSurname` varchar(100) NOT NULL,
+  `clientEmail` text NOT NULL,
+  `clientPhone` varchar(100) DEFAULT NULL,
+  `clientPassword` text NOT NULL,
+  PRIMARY KEY (`clientId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,36 +116,33 @@ LOCK TABLES `services` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `users`
+-- Table structure for table `staff`
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `staff`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `UserId` int NOT NULL AUTO_INCREMENT,
-  `Email` varchar(255) NOT NULL,
-  `PasswordHash` varchar(255) NOT NULL,
-  `FirstName` varchar(100) NOT NULL,
-  `LastName` varchar(100) NOT NULL,
-  `PhoneNumber` varchar(20) DEFAULT NULL,
-  `Role` varchar(20) NOT NULL DEFAULT 'User',
-  `CreatedAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `IsActive` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`UserId`),
-  UNIQUE KEY `Email` (`Email`),
-  CONSTRAINT `users_chk_1` CHECK ((`Role` in (_utf8mb4'Client',_utf8mb4'Manager',_utf8mb4'Master')))
+CREATE TABLE `staff` (
+  `staffId` int NOT NULL AUTO_INCREMENT,
+  `staffName` varchar(100) NOT NULL,
+  `staffSurname` varchar(100) NOT NULL,
+  `staffEmail` text NOT NULL,
+  `staffPhone` varchar(100) DEFAULT NULL,
+  `staffRole` enum('Admin','Manager') DEFAULT NULL,
+  `staffPassword` text NOT NULL,
+  PRIMARY KEY (`staffId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `staff`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -118,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-17 11:21:22
+-- Dump completed on 2025-09-29  8:50:11
